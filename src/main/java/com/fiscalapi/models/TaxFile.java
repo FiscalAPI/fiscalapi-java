c8566dc141a950d69ba5db3db8e7d7403daf767c
JesusMendoza <mendoza.git@gmail.com>
2025-03-10 18:37:46 -0600

    TaxFile service added

--- src/main/java/com/fiscalapi/models/TaxFile.java
+++ src/main/java/com/fiscalapi/models/TaxFile.java
@@ -1,4 +1,80 @@
 package com.fiscalapi.models;
 
+import java.util.Date;
+
 public class TaxFile {
+    private String personId;
+    private String tin;
+    private String base64File;
+    private int fileType;
+    private String password;
+    private Date validFrom;
+    private Date validTo;
+    private Integer sequence;
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getTin() {
+        return tin;
+    }
+
+    public void setTin(String tin) {
+        this.tin = tin;
+    }
+
+    public String getBase64File() {
+        return base64File;
+    }
+
+    public void setBase64File(String base64File) {
+        this.base64File = base64File;
+    }
+
+    public int getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(int fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(Date validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public Date getValidTo() {
+        return validTo;
+    }
+
+    public void setValidTo(Date validTo) {
+        this.validTo = validTo;
+    }
+
+    public Integer getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(Integer sequence) {
+        this.sequence = sequence;
+    }
 }
+
+
